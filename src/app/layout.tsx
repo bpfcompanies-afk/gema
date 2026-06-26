35b1877ded0c7ba7984c02b24f14625d369288fa
BPF <brayan-_2011@hotmail.com>
2026-06-26 18:00:27 -0500

    add analitycs google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import SmoothScroll from "../components/SmoothScroll"; // <--- Importamos aquí
 import { Analytics } from "@vercel/analytics/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -25,6 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <Footer />
         </SmoothScroll>
         <Analytics />
+        <GoogleAnalytics gaId="G-E7K0643F6F" />
       </body>
     </html>
   );
